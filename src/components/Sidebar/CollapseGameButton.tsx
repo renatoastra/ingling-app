8459441339512fb24213da159b0a4af75c1306ac
Renato Costa <renatocostatristao@gmail.com>
2023-08-08 16:50:42 -0300

    justify-center no collapsibleContent

--- src/components/Sidebar/CollapseGameButton.tsx
+++ src/components/Sidebar/CollapseGameButton.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 
 export const CollapseGameButton = () => {
   return (
-    <Collapsible className="w-full">
+    <Collapsible className="w-full ">
       <CollapsibleTrigger asChild className="DialogContent ">
         <Button variant="outline" className="DialogContent w-full">
           <PlayIcon
@@ -19,9 +19,9 @@ export const CollapseGameButton = () => {
       </CollapsibleTrigger>
       <CollapsibleContent className="DialogContent flex w-full flex-col items-center justify-center space-y-2  pt-4 font-mono text-xs  font-bold uppercase animate-in animate-out">
         <p className="">Genshin Impact</p>
-        <div className="relative flex w-full items-center justify-end pt-4">
+        <div className="relative flex w-full items-center justify-center pt-4">
           <p className="">Honkai StarRail</p>
-          <span className="absolute top-2 animate-bounce  pl-0 text-[8px] font-bold text-indigo-300 dark:text-emerald-600">
+          <span className="absolute right-0 top-2 animate-bounce  pl-0 text-[8px] font-bold text-indigo-300 dark:text-emerald-600">
             SOON
           </span>
         </div>
