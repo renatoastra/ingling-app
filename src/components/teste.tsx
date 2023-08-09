52dbc759c0738c57864483e3e8492ad9e01541c6
Renato Costa <renatocostatristao@gmail.com>
2023-08-09 15:57:45 -0300

    removing teste componenet

--- src/components/teste.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import useTranslation from "next-translate/useTranslation";
-
-export const Teste = () => {
-  const { t, lang } = useTranslation("xesque");
-
-  return <h1>{t("title")}</h1>;
-};
